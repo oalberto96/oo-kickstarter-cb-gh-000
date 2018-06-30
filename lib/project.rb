fac281144c5275754f729f34ffbd242435a04d0a
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-30 19:28:51 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+
+
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    backer.back_project(self)
+  end
+
+end
